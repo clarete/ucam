4efd6eec093f27d9bc43927fa2d0af13f7558c5d
Lincoln de Sousa <lincoln@comum.org>
2020-02-24 15:33:34 -0500

    feat(chat): Match user signing in with list in configuratio

--- chat/src/main.rs
+++ chat/src/main.rs
@@ -1,5 +1,6 @@
 use std::io;
 use std::time::{Duration, Instant};
+use std::collections::HashMap;
 
 use actix_rt;
 use actix::prelude::*;
@@ -8,72 +9,6 @@ use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
 use actix_web_actors::ws;
 
 use serde_derive::Deserialize;
-
-
-/// How often heartbeat pings are sent
-const HEARTBEAT_INTERVAL: Duration = Duration::from_secs(5);
-/// How long before lack of client response causes a timeout
-const CLIENT_TIMEOUT: Duration = Duration::from_secs(10);
-
-struct ChatSocket {
-    heartbeat: Instant,
-}
-
-impl ChatSocket {
-    fn new() -> Self {
-        return Self { heartbeat: Instant::now() };
-    }
-
-    fn alive(&mut self) {
-        self.heartbeat = Instant::now();
-    }
-}
-
-/// Define http actor for the ChatSocket struct
-impl Actor for ChatSocket {
-    type Context = ws::WebsocketContext<Self>;
-
-    /// Method is called on actor start.  We start the heartbeat
-    /// process here.
-    fn started(&mut self, ctx: &mut <Self as Actor>::Context) {
-        ctx.run_interval(HEARTBEAT_INTERVAL, |act, ctx| {
-            if Instant::now().duration_since(act.heartbeat) > CLIENT_TIMEOUT {
-                println!("Websocket Client heartbeat failed, disconnecting!");
-                ctx.stop();
-            } else {
-                ctx.ping(b"");
-            }
-        });
-    }
-}
-
-/// Handler for ws::Message message
-impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for ChatSocket {
-    fn handle(
-        &mut self,
-        msg: Result<ws::Message, ws::ProtocolError>,
-        ctx: &mut Self::Context,
-    ) {
-        println!("msg: {:?}", msg);
-
-        match msg {
-            Ok(ws::Message::Ping(msg))   => { self.alive(); ctx.pong(&msg) },
-            Ok(ws::Message::Pong(_))     => { self.alive(); },
-            Ok(ws::Message::Text(text))  => ctx.text(text),
-            Ok(ws::Message::Binary(bin)) => ctx.binary(bin),
-            Ok(ws::Message::Close(_))    => { ctx.stop(); },
-            _ => ctx.stop(),
-        }
-    }
-}
-
-async fn auth() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
-}
-
-async fn ws(req: HttpRequest, stream: web::Payload) -> Result<HttpResponse, actix_web::Error> {
-    ws::start(ChatSocket::new(), &req, stream)
-}
 
 #[derive(Clone, Debug, Deserialize)]
 struct ConfigLogging {
@@ -105,6 +40,85 @@ struct Config {
     logging: Option<ConfigLogging>,
     userauth: ConfigUserAuth,
     locations: HashMap<String, ConfigLocation>,
+}
+
+/// How often heartbeat pings are sent
+const HEARTBEAT_INTERVAL: Duration = Duration::from_secs(5);
+/// How long before lack of client response causes a timeout
+const CLIENT_TIMEOUT: Duration = Duration::from_secs(10);
+
+struct ChatServer {
+    heartbeat: Instant,
+}
+
+impl ChatServer {
+    fn new() -> Self {
+        return Self { heartbeat: Instant::now() };
+    }
+
+    fn alive(&mut self) {
+        self.heartbeat = Instant::now();
+    }
+}
+
+/// Define http actor for the ChatServer struct
+impl Actor for ChatServer {
+    type Context = ws::WebsocketContext<Self>;
+
+    /// Method is called on actor start.  We start the heartbeat
+    /// process here.
+    fn started(&mut self, ctx: &mut <Self as Actor>::Context) {
+        ctx.run_interval(HEARTBEAT_INTERVAL, |act, ctx| {
+            if Instant::now().duration_since(act.heartbeat) > CLIENT_TIMEOUT {
+                println!("Websocket Client heartbeat failed, disconnecting!");
+                ctx.stop();
+            } else {
+                ctx.ping(b"");
+            }
+        });
+    }
+}
+
+/// Handler for ws::Message message
+impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for ChatServer {
+    fn handle(
+        &mut self,
+        msg: Result<ws::Message, ws::ProtocolError>,
+        ctx: &mut Self::Context,
+    ) {
+        println!("msg: {:?}", msg);
+
+        match msg {
+            Ok(ws::Message::Ping(msg))   => { self.alive(); ctx.pong(&msg) },
+            Ok(ws::Message::Pong(_))     => { self.alive(); },
+            Ok(ws::Message::Text(text))  => ctx.text(text),
+            Ok(ws::Message::Binary(bin)) => ctx.binary(bin),
+            Ok(ws::Message::Close(_))    => { ctx.stop(); },
+            _ => ctx.stop(),
+        }
+    }
+}
+
+/// Represents the data that arrives from the authentication form.
+#[derive(Debug, Deserialize)]
+struct AuthForm {
+    user: String,
+}
+
+/// Authenticate the request
+async fn auth(config: web::Data<Config>, body: web::Json<AuthForm>) -> impl Responder {
+    for email in &config.userauth.allowed_emails {
+        if *email == body.user {
+            // TODO: Generate token for user
+            // TODO: Send token via email
+            return HttpResponse::Ok().finish();
+        }
+    }
+    HttpResponse::Unauthorized().body("Unknown Address")
+}
+
+async fn ws(req: HttpRequest, stream: web::Payload) -> Result<HttpResponse, actix_web::Error> {
+    ws::start(ChatServer::new(), &req, stream)
 }
 
 /// Applicatio error types.
