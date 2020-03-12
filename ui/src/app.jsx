0ca71935b7c7edb74b6770d025bc486980796466
Lincoln de Sousa <lincoln@comum.org>
2020-03-12 12:18:31 -0400

    feat(ui): Add the UI for listing clients from server

--- ui/src/app.jsx
+++ ui/src/app.jsx
@@ -11,6 +11,15 @@ import Box from '@material-ui/core/Box';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
+import Divider from '@material-ui/core/Divider';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListSubheader from '@material-ui/core/ListSubheader';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import VideocamIcon from '@material-ui/icons/Videocam';
+import MicIcon from '@material-ui/icons/Mic';
 
 import { useForm } from 'react-hook-form';
 import { store, AuthState } from './store';
@@ -34,10 +43,51 @@ const Error = styled.div`
   color: #ee2200;
 `;
 
+const IconShell = styled.div`
+  width: 24px;
+  height: 24px;
+  float: left;
+  marginRight: 2px;
+`;
+
+const iconStyle = { width: 16, height: 16 };
+
+function ClientItem({ primary, caps }) {
+  return (
+    <ListItem button component="li">
+      <ListItemText primary={primary} />
+      <ListItemSecondaryAction>
+        {caps.map(c =>
+          {['s:audio', 's:video'].includes(c) &&
+           <IconShell key={`key-cap-${primary}-${c}`}>
+             <Avatar>
+               {c === 's:video' && <VideocamIcon style={iconStyle} />}
+               {c === 's:audio' && <MicIcon style={iconStyle} />}
+             </Avatar>
+           </IconShell>})}
+      </ListItemSecondaryAction>
+    </ListItem>
+  );
+}
+
 function MainScreen() {
+  const { api } = React.useContext(store);
+  const [clientList, setClientList] = React.useState({});
+  React.useEffect(() => {
+    api.listClients().then(clients => setClientList(clients));
+  }, []);
   return (
     <CenterCenterShell>
-      Should be something
+      <Container component="main" maxWidth="xs">
+        <h1>What do you want to see</h1>
+        <List>
+          {Object.entries(clientList).map(([jid, caps], i) =>
+            <div key={`key-client-${jid}`}>
+              <ClientItem primary={jid} caps={caps} />
+              <Divider component="li" />
+            </div>)}
+        </List>
+      </Container>
     </CenterCenterShell>
   );
 }
@@ -45,6 +95,7 @@ function MainScreen() {
 function AuthForm() {
   const { api } = React.useContext(store);
   const { register, handleSubmit, errors } = useForm();
+  const onSubmit = async data => api.startSession(data);
 
   return (
     <CenterCenterShell>
@@ -57,7 +108,7 @@ function AuthForm() {
             Sign in
           </Typography>
         </AvatarShell>
-        <form noValidate onSubmit={handleSubmit(api.auth)}>
+        <form noValidate onSubmit={handleSubmit(onSubmit)}>
           <TextField
             variant="outlined"
             margin="normal"
@@ -76,9 +127,9 @@ function AuthForm() {
            </Error>}
 
           {api.authState() === AuthState.Unauthorized &&
-            <Error>
-              Email unauthorized.
-            </Error>}
+           <Error>
+             Email unauthorized.
+           </Error>}
 
           <Button
             type="submit"
