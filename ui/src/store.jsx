f3d17f4e96b9b0cc353a03c9d2fe10567255e7b2
Lincoln de Sousa <lincoln@comum.org>
2020-03-12 11:21:55 -0400

    feat(ui): Websocket client & API to retrieve static client list
    There was also a light refactor of the API from a function to a lil
    class.  The `auth.success' state was also added to 1. set the auth
    state and 2. save the user's JID.

--- ui/src/store.jsx
+++ ui/src/store.jsx
@@ -28,6 +28,13 @@ const createReducer = () => {
       return newState;
     }
 
+    case 'auth.success': {
+      const newState = { ...state };
+      newState.auth.state = AuthState.Authenticated;
+      newState.auth.user = action.jid;
+      return newState;
+    }
+
     default:
       throw new Error(`No action ${action.type}`);
     };
@@ -39,22 +46,29 @@ const createReducer = () => {
 const rnd = new Uint32Array(1); window.crypto.getRandomValues(rnd);
 const resource = rnd.join('');
 
-/** The public API for this storage layer.  */
-const buildAPI = (state, dispatch) => ({
+/** The public API for this storage layer */
+class API {
+  constructor (state, dispatch) {
+    this.state = state;
+    this.dispatch = dispatch;
+  }
+
   /** Retrieve user authentication state */
-  authState: () => state.auth.state,
+  authState() {
+    return this.state.auth.state;
+  };
 
   /** Authenticate a user */
-  auth: async (data) => {
+  async auth(data) {
     // Update UI to show loading spinner
-    dispatch({ type: 'auth.state', data: AuthState.Loading });
+    this.dispatch({ type: 'auth.state', data: AuthState.Loading });
 
     // Build the full user's JID.  That'd allow the same user to
     // access the system from different devices.
     const jid = `${data.email}/${resource}`;
 
     // Actual auth
-    const response = await fetch('/b/auth', {
+    const response = await fetch('/api/auth', {
       method: 'post',
       body: JSON.stringify({ jid }),
       headers: { 'Content-Type': 'application/json' },
@@ -66,18 +80,74 @@ const buildAPI = (state, dispatch) => ({
       400: AuthState.Failed,
       401: AuthState.Unauthorized,
     };
-    dispatch({
-      type: 'auth.state',
-      data: statusToState[response.status],
-    });
-  },
-});
+    const authState = statusToState[response.status];
+    if (authState === AuthState.Authenticated)
+      this.dispatch({ type: 'auth.success', jid });
+    else
+      this.dispatch({ type: 'auth.state', data: authState });
+    return authState;
+  }
+
+  /** Retrieve the list of currently connected clients */
+  async listClients() {
+    const response = await window.fetch('/api/clients');
+    const allClients = await response.json();
+    delete allClients[this.state.auth.user];
+    return allClients;
+  }
+
+  /** Return the URL to connect to the WebSocket */
+  webSocketUrl() {
+    return 'ws://localhost:8080/ws?token=admin@domain.tld';
+  }
+
+  /** Sends a messave to the websocket server */
+  wsSend(message) {
+    this.state.ws.send(JSON.stringify(message));
+  }
+
+  /** Sends this client's capabilities upon successful connection */
+  wsOpen(event) {
+    this.wsSend({ caps: ['r:audio', 'r:video', 's:audio'] });
+  }
+
+  /** Triggered when the server closes the connection */
+  wsClose(event) {
+  }
+
+  /** Triggered upon error on the connection */
+  wsError(event) {
+  }
+
+  /** Event triggered when the server sends this client a message */
+  wsMessage(event) {
+    console.log('event', event);
+  }
+
+  /** Connect to the WebSocket server & bind event callbacks */
+  connect() {
+    this.state.ws = new window.WebSocket(this.webSocketUrl());
+    this.state.ws.addEventListener('open', this.wsOpen.bind(this));
+    this.state.ws.addEventListener('error', this.wsError.bind(this));
+    this.state.ws.addEventListener('message', this.wsMessage.bind(this));
+    this.state.ws.addEventListener('close', this.wsClose.bind(this));
+  }
+
+  /** Entry point for this client's session */
+  async startSession(data) {
+    // Issue the authentication request
+    await this.auth(data);
+    // If we're good, proceed to connecting to the chat server
+    if (this.authState() === AuthState.Authenticated)
+      await this.connect();
+  }
+}
 
 const Provider = ({ children }) => {
   const { Provider } = store;
   const memoizedReducer = React.useCallback(createReducer(), []);
   const [state, dispatch] = useReducer(memoizedReducer, initialState);
-  const api = buildAPI(state, dispatch);
+  const api = new API(state, dispatch);
   return (<Provider value={{ state, dispatch, api }}>{children}</Provider>);
 };
 
