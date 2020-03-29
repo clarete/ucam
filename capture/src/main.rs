c85af3eb54899b28900de42edfc074a3265f829f
Lincoln de Sousa <lincoln@comum.org>
2020-03-29 01:11:40 -0400

    feat,refactor(capture): Start to put GST code to capture video \o/
    This is a very messy commit. It's mixing up a bit of a refactoring
    with the actual new functionality added with gstreamer.  The most
    important parts of the refactoring were:
    
     0. ChatClient isn't a tuple anymore. It's now a struct and will hold
        more fields in the future (this was the initial reason for
        refactoring but I didn't just stop at that :/);
    
     1. Use actix_rt::main and get main to be async as well;
    
     2. Move initialization code for websocket client out of the main
        function (thanks to the above change);
    
     3. Removed one of the impl blocks for ChatClient
    
    The main GST features added here were (mostly copied from the example
    from `centricular' https://github.com/centricular/gstwebrtc-demos)
    
     0. Initialize GST!! \o/;
    
     1. Add a function to check if we've got all the plugins we
        need (gotta update that with the list of plugins we actually care
        about);
    
     2. Initialize both pipeline & webrtcbin element. The pipeline is
        being initialized with the description copied from the example
        as well;

--- capture/src/main.rs
+++ capture/src/main.rs
@@ -1,26 +1,36 @@
 #[macro_use]
 extern crate log;
 #[macro_use]
-extern crate failure;
+extern crate anyhow;
 
-use failure::Error;
+use anyhow::Error;
 
 use std::time::Duration;
 // use std::{io, thread};
 
 use actix::io::SinkWrite;
-use actix::*;
+use actix::prelude::Recipient;
+use actix::{
+    Actor, ActorContext, Arbiter, AsyncContext, Context, Handler, Message, StreamHandler, System,
+};
+
 use actix_codec::Framed;
 use awc::{
-    error::WsProtocolError,
-    ws::{Codec, Frame, Message},
-    BoxedSocket, Client,
+    error::{WsProtocolError},
+    ws::{Codec, Frame, Message as WsMessage},
+    BoxedSocket,
 };
 use bytes::Bytes;
 use futures::stream::{SplitSink, StreamExt};
 
 use serde_derive::{Deserialize, Serialize};
 use serde_json::json;
+
+use gst::gst_element_error;
+use gst::prelude::*;
+
+const STUN_SERVER: &str = "stun://stun.l.google.com:19302";
+const JSWS_SERVER: &str = "ws://127.0.0.1:7070/ws";
 
 #[derive(Deserialize, Debug)]
 struct Envelope {
@@ -44,35 +54,64 @@ enum ClientMessage {
     },
 }
 
-const JSWS_SERVER: &str = "ws://127.0.0.1:7070/ws";
+/// Create the Gstreamer pipeline & grab the webrtcbin element
+fn get_gst_elements() -> Result<(gst::Pipeline, gst::Element), Error> {
+    let pipeline = gst::parse_launch(
+        "videotestsrc pattern=ball is-live=true ! vp8enc deadline=1 ! rtpvp8pay pt=96 ! webrtcbin. \
+         audiotestsrc is-live=true ! opusenc ! rtpopuspay pt=97 ! webrtcbin. \
+         webrtcbin name=webrtcbin"
+    )?;
+    // Downcast from gst::Element to gst::Pipeline
+    let pipeline = pipeline
+        .downcast::<gst::Pipeline>()
+        .expect("not a pipeline");
+    // Get access to the webrtcbin by name
+    let webrtcbin = pipeline
+        .get_by_name("webrtcbin")
+        .expect("can't find webrtcbin");
+    // Set webrtcbin properties
+    webrtcbin.set_property_from_str("stun-server", STUN_SERVER);
+    webrtcbin.set_property_from_str("bundle-policy", "max-bundle");
+    // Return the application with newly created gst stuff and
+    // client picked up from parameters
+    Ok((pipeline, webrtcbin))
+}
 
-fn main() {
+/// Create the HTTP client and connect it to the websocket server.
+/// Then return the framed response
+async fn get_ws_client() -> Result<Framed<BoxedSocket, Codec>, Error> {
+    let client = awc::Client::new()
+        .ws(JSWS_SERVER)
+        .header("Authorization", "Bearer cam001@studio.loc")
+        .connect()
+        .await;
+    match client {
+        Ok((_, framed)) => Ok(framed),
+        Err(e) => bail!("Can't connect to server: {}", e),
+    }
+}
+
+#[actix_rt::main]
+async fn main() -> Result<(), Error> {
+    // Initialize the logging stuff
     ::std::env::set_var("RUST_LOG", "awc=debug,actix=debug,actix_web=debug");
     env_logger::init();
+    // Initialize Gstreamer stuff
+    gst::init()?;
+    check_plugins()?;
+    // Build instances of websocket client and then gstreamer stuff
+    let wsclient = get_ws_client().await?;
+    let (pipeline, webrtcbin) = get_gst_elements()?;
 
-    let sys = System::new("websocket-client");
-
-    Arbiter::spawn(async {
-        let error_handler = |e| {
-            println!("Error: {}", e);
-        };
-
-        let (_, framed) = Client::new()
-            .ws(JSWS_SERVER)
-            .header("Authorization", "Bearer cam001@studio.loc")
-            .connect()
-            .await
-            .map_err(error_handler)
-            .unwrap();
-
-        let (sink, stream) = framed.split();
+    Arbiter::spawn(async move {
+        let (sink, stream) = wsclient.split();
         let addr = ChatClient::create(|ctx| {
             ChatClient::add_stream(stream, ctx);
-            ChatClient(SinkWrite::new(sink, ctx))
+            ChatClient {
+                framed: SinkWrite::new(sink, ctx),
+            }
         });
 
-        // Send the initial message with the capabilities of this
-        // client.
         addr.do_send(ClientCommand(
             json!({
                 "caps": [
@@ -83,25 +122,39 @@ fn main() {
             })
             .to_string(),
         ));
-
-        println!("Not really sure what I'm doing");
-
-        // Wait indefinitely for new messages from either the
-        // websocket connection or the gstreamer bus
     });
-    sys.run().unwrap();
+
+    actix_rt::signal::ctrl_c()
+        .await
+        .expect("failed to listen for event");
+
+    Ok(())
 }
 
-struct ChatClient(SinkWrite<Message, SplitSink<Framed<BoxedSocket, Codec>, Message>>);
+struct ChatClient {
+    framed: SinkWrite<WsMessage, SplitSink<Framed<BoxedSocket, Codec>, WsMessage>>,
+}
 
 impl ChatClient {
+    fn hb(&self, ctx: &mut Context<Self>) {
+        ctx.run_later(Duration::new(1, 0), |act, ctx| {
+            act.framed
+                .write(WsMessage::Ping(Bytes::from_static(b"")))
+                .unwrap();
+            act.hb(ctx);
+
+            // client should also check for a timeout here, similar to the
+            // server code
+        });
+    }
+
     fn handle_websocket_message(&self, msg: &Result<Frame, WsProtocolError>) -> Result<(), Error> {
         if let Ok(Frame::Text(txt)) = msg {
             let utf8 = std::str::from_utf8(&txt)?;
             let envelope: Envelope = serde_json::from_str(utf8)?;
             let message: ClientMessage = serde_json::from_str(envelope.message.as_str())?;
             match message {
-                ClientMessage::Sdp { type_, sdp } => self.sdp(&type_, &sdp),
+                ClientMessage::Sdp { type_, sdp } => self.sdp(&type_, &sdp)?,
                 ClientMessage::Ice {
                     sdp_mline_index,
                     candidate,
@@ -112,8 +165,15 @@ impl ChatClient {
         bail!("Not Text")
     }
 
-    fn sdp(&self, type_: &str, sdp: &str) {
-        println!("Received SDP: {} {}", type_, sdp);
+    fn sdp(&self, type_: &str, sdp: &str) -> Result<(), Error> {
+        if type_ == "offer" {
+            println!("Received SDP offer:\n{}\n", sdp);
+
+            if let Ok(ret) = gst_sdp::SDPMessage::parse_buffer(sdp.as_bytes()) {}
+
+            // .map_err(|_| bail!("Failed to parse SDP offer"));
+        }
+        Ok(())
     }
 
     fn ice(&self, sdp_mline_index: u32, candidate: &str) {
@@ -141,24 +201,12 @@ impl Actor for ChatClient {
     }
 }
 
-impl ChatClient {
-    fn hb(&self, ctx: &mut Context<Self>) {
-        ctx.run_later(Duration::new(1, 0), |act, ctx| {
-            act.0.write(Message::Ping(Bytes::from_static(b""))).unwrap();
-            act.hb(ctx);
-
-            // client should also check for a timeout here, similar to the
-            // server code
-        });
-    }
-}
-
 /// Handle sending messages
 impl Handler<ClientCommand> for ChatClient {
     type Result = ();
 
     fn handle(&mut self, msg: ClientCommand, _ctx: &mut Context<Self>) {
-        self.0.write(Message::Text(msg.0)).unwrap();
+        self.framed.write(WsMessage::Text(msg.0)).unwrap();
     }
 }
 
@@ -182,3 +230,37 @@ impl StreamHandler<Result<Frame, WsProtocolError>> for ChatClient {
 }
 
 impl actix::io::WriteHandler<WsProtocolError> for ChatClient {}
+
+fn check_plugins() -> Result<(), Error> {
+    let needed = [
+        "videotestsrc",
+        "audiotestsrc",
+        "videoconvert",
+        "audioconvert",
+        "autodetect",
+        "opus",
+        "vpx",
+        "webrtc",
+        "nice",
+        "dtls",
+        "srtp",
+        "rtpmanager",
+        "rtp",
+        "playback",
+        "videoscale",
+        "audioresample",
+    ];
+
+    let registry = gst::Registry::get();
+    let missing = needed
+        .iter()
+        .filter(|n| registry.find_plugin(n).is_none())
+        .cloned()
+        .collect::<Vec<_>>();
+
+    if !missing.is_empty() {
+        bail!("Missing plugins: {:?}", missing);
+    } else {
+        Ok(())
+    }
+}
